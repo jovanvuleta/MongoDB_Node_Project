1c21bd615efc13592250b1975e4f27d2d568b120
saras <lost.func@gmail.com>
2020-03-10 18:10:42 +0100

    fixed init errors

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const { app, dbcon, mongo } = require('./core/app.config.js').AppConfig();
 require('./controllers/main.controller.js').MainController(app, dbcon, mongo);
 require('./controllers/state.controller.js').StateController(app, dbcon, mongo);
 require('./controllers/populatedPlace.controller.js').PopulatedPlaceController(app, dbcon, mongo);
-
-// U KURAC
---
+++ models/mongodb/state.model.js
@@ -0,0 +1,3 @@
+exports.StateCollectionModel = function(mongo) {
+    
+}
