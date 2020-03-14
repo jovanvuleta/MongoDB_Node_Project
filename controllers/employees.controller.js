b7089e38c74bcf98d211462a58b1b9b7169d25bc
jocavuleta <jocavuleta@email.com>
2020-03-14 22:50:46 +0100

    Added delete option and edit option final

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,19 +1,6 @@
 exports.EmployeesControler = (app, dbcon) => {
     const employeesModel = require('../models/mysql/employees.model.js').Employees(dbcon);
-
-    app.get('/getAllEmployees', (req, res) => {
-        employeesModel.getAllEmployees()
-            .then((data) => {
-                res.render('employees', {
-                    employees: data
-                })
-            })
-            .catch((err) => {
-                res.render('message', {
-                    errorMessage: 'Error' + err
-                })
-            });
-    });
+    const institutionModel = require('../models/mysql/institution.model.js').InstitutionModel(dbcon);
 
     app.get('/getAllEmployeesByInstitution/:id', (req, res) => {
         employeesModel.getAllEmployeesByInstitution(req.params.id)   //Call amoel function that return all states from the database
@@ -47,8 +34,8 @@ exports.EmployeesControler = (app, dbcon) => {
             })
     });
 
-    app.post('/addEmployee/:id', (req, res) => {
-        employeesModel.addEmployee(req.body.info_type, req.body.info_id, req.body.employeeId, req.body.employeeSurname, req.body.employeeMidLetter, req.body.employeeName)
+    app.post('/addEmployee/:id/:type', (req, res) => {
+        employeesModel.addEmployee(req.params.type, req.params.id, req.body.employeeId, req.body.employeeSurname, req.body.employeeMidLetter, req.body.employeeName)
             .then((data) => {
                 res.render('message', {  //after successfully excuting the query, render the 'message.ejs' view in order to display the message
                     successMessage: 'Employee ' + req.body.employeeName + ' was added successfully.',   //success message
@@ -61,6 +48,57 @@ exports.EmployeesControler = (app, dbcon) => {
                     link: '<a href="/addInstitution"> Go Back</a>'
                 });
             })
+    });
+
+    app.get('/deleteEmployee/:id', (req, res) => {
+        employeesModel.deleteEmployee(req.params.id)
+            .then((data) => {
+                res.render('message', {
+                    successMessage: 'Employee ' + req.params.id + ' was deleted successfully.',   //success message
+                    link: '<a href="/getAllInstitutions/"> Go Back</a>',  //provide a link that provides a links to another page
+                });
+            })
+            .catch((err) => {
+                res.render('message', {
+                    errorMessage: 'ERROR: ' + err,
+                    link: '<a href="/addEmployee/:id/:type"> Go Back</a>'
+                })
+            })
+    });
+
+    app.get('/editEmployee/:type_ins/:vu_id/:emp_id', (req, res) => {
+        let getAllTypes = institutionModel.getAllTypes().then();
+        let getEmployees = employeesModel.getAllEmployeesByInstitutionAndEmployeeId(req.params.vu_id, req.params.emp_id).then();
+
+        //Retrieves state's data in order to show the intinal data of the requested state to be dited
+        Promise.all([getAllTypes, getEmployees]).then((data) => {
+            console.log("logged data:");
+            console.log(data[1][0]);
+            res.render('editEmployee', {
+                types: data[0],
+                employee: data[1][0]
+            });
+        })
+            .catch((err) => {
+                res.send('editEmployee', err);
+            });
+    });
+
+    app.post('/editEmployee/:type_ins/:vu_id/:emp_id', (req, res) => {
+        employeesModel.editEmployee(req.params.type_ins, req.body.employeeSurname, req.body.employeeMiddleLetter, req.body.employeeName, req.params.vu_id, req.params.emp_id)
+            .then((data) => {
+                console.log(data);
+                res.render('message', {
+                    successMessage: 'Employee ' + req.body.employeeName + ' was edited successfully!',  //success message
+                    link: '<a href="/getAllInstitutions"> Go back!</a>'      //provide a link that provides a links to another page
+                });
+            })
+            .catch((err) => {
+                res.render('message', {      //In case the query fail. render 'message.ejs' and display the obtained error message
+                    errorMessage: 'ERROR: ' + err,
+                    link: '<a href="/editEmployee/' + req.params.vu_id + "/" + req.params.emp_id + ' "> Go back!</a>'
+                });
+            });
     });
 
 }
--- models/mysql/employees.model.js
+++ models/mysql/employees.model.js
@@ -28,6 +28,19 @@ exports.Employees = (dbcon) => {
                 });
             })
         },
+        getAllEmployeesByInstitutionAndEmployeeId: (vu_id, emp_id) => {
+            return new Promise((resolve, reject) => {
+                let query = "SELECT * FROM EMPLOYEES WHERE VU_IDENTIFIKATOR LIKE ? AND ZAP_REDNI_BROJ LIKE ?;";
+                dbcon.query(query, [vu_id, emp_id], (err, data) => {
+                    if (!err) {
+                        resolve(data);
+                    } else {
+                        reject(err);
+                        console.log(err);
+                    }
+                });
+            })
+        },
 
         addEmployee: (TIP_UST, VU_IDENTIFIKATOR, ZAP_REDNI_BROJ, ZAP_PREZIME, ZAP_SREDNJE_SLOVO, ZAP_IME) => {
             return new Promise((resolve, reject) => {
@@ -55,6 +68,31 @@ exports.Employees = (dbcon) => {
                     }
                 });
             });
+        },
+        deleteEmployee: (id) => {
+            return new Promise((resolve, reject) => {
+                let query = 'DELETE FROM EMPLOYEES WHERE ZAP_REDNI_BROJ LIKE ?;';
+                dbcon.query(query, [id], (err, data) => {
+                    if (!err) {
+                        resolve(data);
+                    } else {
+                        reject(err);
+                        console.log(err);
+                    }
+                })
+            });
+        },
+        editEmployee: (type, surname, middleLetter, name, vu_id, emp_id) => {
+            return new Promise((resolve, reject) => {
+                let query = "UPDATE EMPLOYEES SET TIP_UST = ?, ZAP_PREZIME = ?, ZAP_SREDNJE_SLOVO = ?, ZAP_IME = ? WHERE (VU_IDENTIFIKATOR = ? AND ZAP_REDNI_BROJ = ?);";
+                dbcon.query(query, [type, surname, middleLetter, name, vu_id, emp_id], (err, data) => {
+                    if (!err) {
+                        resolve(data);
+                    } else {
+                        reject(err);
+                    }
+                })
+            })
         }
     }
 }
